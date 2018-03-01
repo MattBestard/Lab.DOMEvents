3ef9730f9d1a8d110873a7daf917f793ad9602cc
Matthew Bestard <matthew.r.bestard@gmail.com>
2018-03-01 15:10:45 -0500

    initial commit

---
+++ draggable.js
@@ -0,0 +1,15 @@
+function Mdown(event) {
+    
+}
+
+function Mup (event) {
+    
+}
+
+function Mmove(event) {
+    
+}
+
+document.addEventListener('mousedown', event);
+document.addEventListener('mousemove', event);
+document.addEventListener('mouseup', event);
