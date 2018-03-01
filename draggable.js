e67693e4f74df3e832d196ded2b22d6aebda0813
Matthew Bestard <matthew.r.bestard@gmail.com>
2018-03-01 15:16:36 -0500

    made functioning event handlers

--- draggable.js
+++ draggable.js
@@ -1,15 +1,15 @@
 function Mdown(event) {
-    
+    console.log('mousedown', event);
 }
 
 function Mup (event) {
-    
+    console.log('mouseup', event);
 }
 
 function Mmove(event) {
-    
+    console.log('mousemove', event);
 }
 
-document.addEventListener('mousedown', event);
-document.addEventListener('mousemove', event);
-document.addEventListener('mouseup', event);
+document.addEventListener('mousedown', Mdown);
+document.addEventListener('mousemove', Mmove);
+document.addEventListener('mouseup', Mup);
